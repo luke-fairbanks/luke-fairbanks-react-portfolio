88830135a74762ba4fa8e9fa28ea3187d122b084
Luke Fairbanks <luke@Hunters-Air.hsd1.ut.comcast.net>
2020-04-25 18:11:36 -0600

    tryin

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/luke-face.jpg"
+import profilePicture from "../../static/assets/images/bio/luke-face.jpg"
 
 export default function() {
     return (
