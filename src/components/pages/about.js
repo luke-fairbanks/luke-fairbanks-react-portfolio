f191472e46dd461e8018923469ccae8b41d347be
Luke Fairbanks <luke@Hunters-Air.hsd1.ut.comcast.net>
2020-04-30 14:25:58 -0600

    commit for about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,7 +13,7 @@ export default function() {
             }}
             />
             <div className="right-column"> 
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At urna condimentum mattis pellentesque id nibh tortor. Viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Amet volutpat consequat mauris nunc congue. Tellus elementum sagittis vitae et leo duis ut. Fringilla est ullamcorper eget nulla. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Elit duis tristique sollicitudin nibh. Viverra justo nec ultrices dui. Egestas sed sed risus pretium quam. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Habitant morbi tristique senectus et netus et malesuada. At urna condimentum mattis pellentesque id nibh. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
+                what up my name is luke fairbanks im 16 years old and i created this website. i live in utah and i play on my highschools soccer team. i take interest in physcics and astronomy in school and i very much enjoy procrastinating. life is good here and i hope one day i can get a gnarly job coding some cool stuff.
             </div>
         </div>
     )
